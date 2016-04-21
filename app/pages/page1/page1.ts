58616839b6808a5be514575788882a8f273b7246
nelson <nelsonec87@gmail.com>
2016-04-20 22:36:44 -0300

    fixes #1 fixes #4

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,12 +1,10 @@
 import {Page} from 'ionic-angular';
 import {MeteorComponent} from 'angular2-meteor';
 import {Parties} from './parties';
-import {MongoCursorDifferFactory} from 'angular2-meteor/mongo_cursor_differ';
 import {IterableDiffers, Component} from 'angular2/core';
 
 @Page({
-    templateUrl: 'build/pages/page1/page1.html',
-    providers: [IterableDiffers.extend([new MongoCursorDifferFactory()])]
+    templateUrl: 'build/pages/page1/page1.html'
 })
 export class Page1 extends MeteorComponent {
     public myData: Mongo.Cursor<any>;
